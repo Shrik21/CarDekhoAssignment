de3638b2f1ae5a726d6c2ef6fe950dd0b72753f7
shrik121 <shreyansh.kumar32@outlook.com>
2026-07-02 09:25:56 +0530

    Fix backend local startup

--- backend/src/main/java/com/cardekho/advisor/car/CarRepository.java
+++ backend/src/main/java/com/cardekho/advisor/car/CarRepository.java
@@ -1,6 +1,17 @@
 package com.cardekho.advisor.car;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CarRepository extends JpaRepository<Car, Long> {
+    @Override
+    @EntityGraph(attributePaths = "features")
+    List<Car> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = "features")
+    Optional<Car> findById(Long id);
 }
